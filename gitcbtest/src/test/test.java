620cefd8291ddfdcfdb58e51dda00012e3c0d8b7
Administrator <Administrator@cb.digiteki.com>
2024-06-21 13:33:41 +0900

    "#2823 add"

--- gitcbtest/src/test/test.java
+++ gitcbtest/src/test/test.java
@@ -7,7 +7,6 @@ public class test {
 		System.out.println("hello");
 		System.out.println("hello");
 		System.out.println("hello");
-		System.out.println("hello");
 		System.out.println("no");
 		System.out.println("no");
 		System.out.println("no");
